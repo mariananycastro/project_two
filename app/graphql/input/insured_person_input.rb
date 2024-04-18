d6b7536134433bdeea0ffa6eb1c43c575323d683
mariana castro <mariananycastro@gmail.com>
2024-04-18 15:51:30 -0300

    add email to insured person

--- app/graphql/input/insured_person_input.rb
+++ app/graphql/input/insured_person_input.rb
@@ -4,5 +4,6 @@ module Input
   class InsuredPersonInput < GraphQL::Schema::InputObject
     argument :name,       String,    required: true
     argument :document,   String,    required: true
+    argument :email,      String,    required: true
   end
 end
