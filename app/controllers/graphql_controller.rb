162940127bd9ed9fcd47ffed13121eb4cf4d0e0a
mariana castro <mariananycastro@gmail.com>
2024-04-24 17:59:02 -0300

    fix tests

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -4,13 +4,12 @@ require 'net/http'
 class GraphqlController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-
   def execute
     query = params[:query]
     jwt_token = request.headers['Authorization']&.split(' ')&.last
     decoded_token = JWT.decode(jwt_token, ENV['JWT_SECRET'], true, algorithm: ENV['JWT_ALGORITHM'])
 
-    result = AppSchema.execute(query, variables: variables)
+    result = AppSchema.execute(query)
     render json: result
   rescue StandardError => e
     handle_error(e)
