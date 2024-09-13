ed70262adb757fe3201e8623bc596e675dd20617
Mariana Naomi Yamada Castro <mariananycastro@gmail.com>
2024-09-13 14:35:50 -0300

    clean empty spaces

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
-	@docker-compose build
+	@docker compose build
 
 bash:
 	docker compose up -d
--- app/graphql/resolvers/policies_by_email_resolver.rb
+++ app/graphql/resolvers/policies_by_email_resolver.rb
@@ -4,11 +4,11 @@ module Resolvers
 
     type [Types::PolicyType], null: true
     argument :email, String, required: true
-    
+
     def resolve(email:)
       path = "insured_person/#{email}"
       response = Clients::AppOneClient.execute(path)
-      
+
       response.body.empty? ? [] : JSON.parse(response.body)
     end
   end
